37558b1e700819af418ca49c4b085a75a6cbdc9b
Makas_M <antoniomacaringue27@gmail.com>
2022-12-26 15:28:35 +0200

    adicionar foto do usuario

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 
 //dados de entrada que o usuario digitou
 var maintable = document.getElementById('maintable'),
-    //botao de baixar pdf
-    pdfout = document.getElementById('pdfout');
+//botao de baixar pdf
+pdfout = document.getElementById('pdfout');
+//acesso a foto e ao inputo do usuratio
+let foto = document.getElementById('imgFoto');
+let file = document.getElementById('flFoto');
+//abrir o input ao clicar na foto
+foto.addEventListener('click', ()=>{file.click();});
+//
+file.addEventListener('change', (event)=>{
+    let ler = new FileReader();
+    ler.onload = () => {
+        foto.src = ler.result;
+    }
+ler.readAsDataURL(file.files[0]);
+})
 
 //funcionalidade para quando o usuario clicar o botao de baixar pdf 
 pdfout.onclick = function () {
-    var doc = new jsPDF('p', 'pt', 'a4');
-    doc.setFontSize(15);
-    var margin = 20;
+
+
+
+
+
+
+    var doc = new jsPDF('p', 'pt', 'A4');
+    
+    var margin = 40;
     var scale = (doc.internal.pageSize.width - margin * 2) / document.body.clientWidth;
     var scale_mobile = (doc.internal.pageSize.width - margin * 2) / document.body.getBoundingClientRect();
 
